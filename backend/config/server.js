432a095a9979f958993c3f19d342a3c86ad1f7ad
BeltranDaniel <beltran_daniel@javeriana.edu.co>
2021-10-21 23:20:55 -0500

    Uploaded CMS managed website

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '81f7f766091ced6ab831013aece211ae'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '9c0413ab-8566-45ee-a56a-5e02dcebd751'
+};
